b07f1e6ecc1a5c936eed910b5e40374ce3819068
pbiecek <przemyslaw.biecek@gmail.com>
2015-05-02 20:21:03 +0200

    by default use mixed model

--- DendriticSpineR/R/plot_crossed_effects.R
+++ DendriticSpineR/R/plot_crossed_effects.R
@@ -9,20 +9,29 @@
 #' @param f1 first effect
 #' @param f2 second effect
 #' @param strat stratification (Animal)
+#' @param mixed if TRUE a mixed model is applied, if FALSE it's just standard linear regression
 #'
 #' @import ggplot2
+#' @import lme4
 #' @import dplyr
 #'
 #' @export
 
-plot_crossed_effects <- function(data, var, trans = I, inv = I, f1="group", f2="condition", strat = "Animal") {
+plot_crossed_effects <- function(data, var, trans = I, inv = I, f1="group", f2="condition", strat = "Animal", mixed=TRUE) {
   ndata <- data
   ndata[,var] <- trans(data[,var])
-  form <- as.formula(paste0(var, " ~ ", f1, ":", f2, "/factor(",strat,")-1"))
-  model <- summary(lm(form, ndata))
 
-  co2 <- data.frame(n = rownames(model$coef), model$coef[,1:2])
-  co2 <- co2[!grepl(co2$n, pattern = "factor(", fixed = TRUE),]
+  if (mixed) {
+    form <- as.formula(paste0(var, " ~ ", f1, ":", f2, "-1 +(1|",strat,")"))
+    ss <- summary(lmer(form, ndata))$coef[,1:2]
+    co2 <- data.frame(n = rownames(ss), ss)
+  } else {
+    form <- as.formula(paste0(var, " ~ ", f1, ":", f2, "/factor(",strat,")-1"))
+    model <- summary(lm(form, ndata))
+    co2 <- data.frame(n = rownames(model$coef), model$coef[,1:2])
+    co2 <- co2[!grepl(co2$n, pattern = "factor(", fixed = TRUE),]
+  }
+
   co2 <- co2 %>% dplyr::mutate(l1 = inv(Estimate - Std..Error),
                   l2 = inv(Estimate),
                   l3 = inv(Estimate + Std..Error)) %>%
