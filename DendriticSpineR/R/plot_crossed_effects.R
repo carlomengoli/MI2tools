e21194cfa349c1eeed6c0d7e9770ac77f703e51d
pbiecek <przemyslaw.biecek@gmail.com>
2015-07-22 07:49:20 +0200

    points will be added to the plot if needed

--- DendriticSpineR/R/plot_crossed_effects.R
+++ DendriticSpineR/R/plot_crossed_effects.R
@@ -11,6 +11,7 @@
 #' @param f2 second effect
 #' @param strat stratification (Animal)
 #' @param mixed if TRUE a mixed model is applied, if FALSE it's just standard linear regression
+#' @param addpoints if TRUE additional points for each f1/f2/strat group will be added
 #'
 #' @import ggplot2
 #' @import lme4
@@ -18,10 +19,11 @@
 #'
 #' @export
 
-plot_crossed_effects <- function(data, var, trans = I, inv = I, f1="group", f2="condition", strat = "Animal", mixed=TRUE) {
+plot_crossed_effects <- function(data, var, trans = I, inv = I, f1="group", f2="condition", 
+                                 strat = "Animal", mixed=TRUE, addpoints=FALSE) {
   ndata <- data
   ndata[,var] <- trans(data[,var])
-
+  
   if (mixed) {
     form <- as.formula(paste0(var, " ~ ", f1, ":", f2, "-1 +(1|",strat,")"))
     ss <- summary(lmer(form, ndata))$coef[,1:2]
@@ -38,8 +40,25 @@ plot_crossed_effects <- function(data, var, trans = I, inv = I, f1="group", f2="
                   l3 = inv(Estimate + Std..Error)) %>%
     extract(n, c(f1, f2), paste0(f1, "([[:alnum:]]+):", f2, "([[:alnum:]]+)"))
 
-  ggplot(co2, aes_string(x=f1, color=f2, ymin="l1", y="l2", ymax="l3")) +
+  pl <- ggplot(co2, aes_string(x=f1, color=f2, ymin="l1", y="l2", ymax="l3")) +
     geom_errorbar(width=0.1, position=position_dodge(width=0.2)) +
     geom_point(size=5, position=position_dodge(width=0.2)) +
     theme_bw() + ylab(var)
+  
+  if (addpoints) {
+    fdata <- data
+    if (length(grep(strat, pattern = ":")) > 0) 
+      stop("the strat argument cannot contain ':' when ppoints are added")
+    fdata$fake_var <- fdata[,var]
+    fdata$fake_f1 <- fdata[,f1]
+    fdata$fake_f2 <- fdata[,f2]
+    fdata$fake_strat <- fdata[,strat]
+    fpoints <- fdata %>%
+      group_by(fake_f1,fake_f2,fake_strat) %>%
+      summarise(meds = mean(fake_var))
+    pl <- pl + 
+      geom_point(data=fpoints, aes(x=fake_f1, color=fake_f2, y=meds, ymin=meds, ymax=meds))
+  }
+
+  pl
 }
