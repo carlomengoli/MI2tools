5c341bf1b9d1d3578b4aec0ae28bd66edea7c054
pbiecek <przemyslaw.biecek@gmail.com>
2015-08-06 09:23:50 +0200

    stemR do projektu z WCO

---
+++ stemR/R/doEdgeR.R
@@ -0,0 +1,17 @@
+#' Function performs edgeR two-sample alculations
+#'
+#' @param counts numeric matrix of read counts.
+#' @param group vector or factor giving the experimental group/condition for each sample/library.
+#'
+#' @import edgeR
+#'
+#' @export
+
+doEdgeR <- function(counts, group) {
+  y <- DGEList(counts=counts, group=group)
+  y <- calcNormFactors(y)
+  y <- estimateCommonDisp(y)
+  y <- estimateTagwiseDisp(y)
+  et <- exactTest(y)
+  et
+}
